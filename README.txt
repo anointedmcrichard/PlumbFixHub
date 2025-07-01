fa4e953c696b80e0602298e6e73d158c5c283753
anointedmcrichard <anointedmcrichard@gmail.com>
2025-07-01 22:58:14 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+Admin Panel for PlumbFix Hub
+
+Password: NUWAMANYA2002
--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-
-function checkPassword() {
-  const password = document.getElementById('password').value;
-  if (password === "NUWAMANYA2002") {
-    alert("Access granted!");
-    window.location.href = "dashboard.html";
-  } else {
-    alert("Incorrect password!");
-  }
-  return false;
-}
+console.log('Admin Panel Loaded');
